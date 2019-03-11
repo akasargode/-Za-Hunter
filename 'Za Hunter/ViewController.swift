c42f3742118c4f54e531de8265ab4ea36960ceb0
Ashwin Kasargode <akasargode@Ashwins-MacBook-Air-3.local>
2019-03-11 11:18:59 -0500

    plist stuff

--- 'Za Hunter/ViewController.swift
+++ 'Za Hunter/ViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
-
+import MapKit
+import CoreLocation
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
