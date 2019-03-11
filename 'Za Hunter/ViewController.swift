e3255cdaaca80ddc522210441da0a1873ba1fcd6
Ashwin Kasargode <akasargode@Ashwins-MacBook-Air-3.local>
2019-03-11 11:21:38 -0500

    added user location

--- 'Za Hunter/ViewController.swift
+++ 'Za Hunter/ViewController.swift
@@ -11,10 +11,13 @@ import MapKit
 import CoreLocation
 class ViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.requestWhenInUseAuthorization()
+        mapView.showsUserLocation = true
     }
 
 
